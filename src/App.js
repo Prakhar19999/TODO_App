2ad9695424b43d99f51fe5e36ea62b456a378a51
Prakhar19999 <prakhar.srivastava19999@gmail.com>
2021-04-22 16:34:48 +0530

    Read from firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {db} from './firebase_config';
 import firebase from 'firebase'
 
 function App() {
 
   const [todoInput,setTodoInput]=useState('');
+  const [todos,setTodos]=useState([]);
 
   const addTodo=(e)=>{
     e.preventDefault();
@@ -16,6 +17,24 @@ function App() {
       inprogress:true,
       timestamp:firebase.firestore.FieldValue.serverTimestamp(),
       todo:todoInput,
+    });
+
+    setTodoInput('');
+  }
+
+  useEffect(()=>{
+    getTodos();
+  },[])
+
+  const getTodos=()=>{
+    db.collection("todos").onSnapshot(function(querySnapshot){
+      setTodos(
+        querySnapshot.docs.map((doc)=>({
+          id:doc.id,
+          todo:doc.data().todo,
+          inprogress:doc.data().inprogress
+        }))
+      );
     });
   }
 
@@ -43,6 +62,11 @@ function App() {
                   onClick={addTodo}
                   style={{display:"none"}}>Submit</Button>
         </form>
+
+        {todos.map((todo)=>(
+          <p>{todo.todo}</p>
+        ))}
+
         </div>
     </div>
   );
